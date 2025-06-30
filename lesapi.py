5c82f6b6742fff97a163664f1564095781674f75
Nikososos <nickywolf@live.nl>
2025-06-30 13:16:24 +0200

    Functie van euro naar usd converter aangepast naar get exchange rate en bijbehorende logica

--- lesapi.py
+++ lesapi.py
@@ -5,15 +5,15 @@ import os
 load_dotenv()
 api_key = os.getenv("api_key")
 
-url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/EUR"
+url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/{base_currency}"
 params ={"appid": api_key}
 
-print("API KEY: " , api_key)
+# print("API KEY: " , api_key)
 
 if api_key is None:
     raise ValueError("API key niet gevonden? Gaat dit wel goed?")
 
-def get_eur_to_usd_rate():
+def get_exchange_rate(base_currency, target_currency):
     response = requests.get(url)
     data = response.json()
     # print("Status code:", response.status_code)  
@@ -24,21 +24,23 @@ def get_eur_to_usd_rate():
     #     print(" Response is not valid JSON.")
     #     return None
 
-    if response.status_code == 200 and "conversion_rates" in data:
-        if "USD" in data["conversion_rates"]:
-            return data["conversion_rates"]["USD"]
+    if response.status_code == 200 and data.get("result") == "success":
+        rate = data["conversion_rates"].get(target_currency.upper())
+        if rate:
+            return rate
         else:
-            print(" USD rate not found in conversion_rates.")
+            raise ValueError(f"Currency '{target_currency}' niet gevonden in de exchange.")
     else:
-        print(" Unexpected API response format or error.")
-    return None
+        print("Er is een ander probleem met je programma, check je api response met de uitgecommende code bovenin")
 
-def convert_eur_to_usd(amount, exchange_rate):
+def convert_currency(amount, exchange_rate):
     return amount * exchange_rate
 
-eur_amount = float(input("Voer het bedrag in EUR in: ").strip())
-exchange_rate = get_eur_to_usd_rate()
-usd_amount = convert_eur_to_usd(eur_amount, exchange_rate)
+base_currency = input("Van welke valuta wil je omrekenen? (bijv. EUR): ").strip().upper()
+target = input("Naar welke valuta wil je omrekenen? (bijv. USD): ").strip().upper()
+amount = float(input(f"Voer het bedrag in {base_currency} in: ").strip())
+exchange_rate = get_exchange_rate(base_currency, target)
+converted = convert_currency(amount, exchange_rate)
 
 print(f"{eur_amount} EUR is gelijk aan {usd_amount:.2f} USD (Wisselkoers: 1 EUR = {exchange_rate} USD)")
 
